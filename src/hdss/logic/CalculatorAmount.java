392a5efac4494f2d76d78a90b8a3f2ff1ea8b695
LABINF\a0330758CORPS <100330758@alumsnos.uc3m.es>
2017-05-18 13:07:57 +0200

    Locuras

---
+++ src/hdss/logic/CalculatorAmount.java
@@ -0,0 +1,12 @@
+package hdss.logic;
+
+import hdss.exceptions.HydricDSSException;
+import hdss.data.AmountAvailableInternalData;
+
+public class CalculatorAmount {
+
+  public String Calculate (Object obj) throws HydricDSSException {
+    AmountAvailableInternalData data = (AmountAvailableInternalData) obj;
+    
+  }
+}
